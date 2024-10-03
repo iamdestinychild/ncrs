785de35cd040606d1a8edd7fbf6c76f30090ebb3
iamdestinychild <omoruyidestiny30@gmail.com>
2024-10-03 16:50:02 +0100

    fix

--- src/app/departments/water-resources-climate/page.jsx
+++ src/app/departments/water-resources-climate/page.jsx
@@ -23,13 +23,25 @@ export default function Department(){
                 </div>
             </section>
             <section className={styles.section_two}>
+                <div className={styles.section_two_inner}>
+
                 <h1>WATER RESOURCES AND CLIMATE</h1>
                 <div className={styles.content}>
-                <div className={styles.banner_cover}>
+                
+                    <p>The earth is the only planet that appears blue in color, also nicknamed the blue planet because 95%of it is covered by water. The department is so named because the interaction of water and climate has its complexity on the earth. Water sometimes brings destruction and sometimes where needed is scarce. Global warming has also affected weather of different parts of the earth. Research into the phenomenon of both interactions is invaluable to human existence.</p>
+                    <div className={styles.banner_cover}>
                         <Image src='/images/img-20.jpg' alt='image' className={styles.banner} fill/>
                     </div>
-                    <p>The earth is the only planet that appears blue in color, also nicknamed the blue planet because 95%of it is covered by water. The department is so named because the interaction of water and climate has its complexity on the earth. Water sometimes brings destruction and sometimes where needed is scarce. Global warming has also affected weather of different parts of the earth. Research into the phenomenon of both interactions is invaluable to human existence.</p>
-                    
+                </div>
+                <div className={styles.card}>
+                    <div className={styles.card_image_cover}>
+                        <Image src='/images/person-10.jpg' alt='image' className={styles.card_image} fill/>
+                    </div>
+                    <div className={styles.card_details}>
+                        <h1>Yehoshua gwongtok nyamdang</h1>
+                        <p>Head of department</p>
+                    </div>
+                </div>
                 </div>
             </section>
         </main>
