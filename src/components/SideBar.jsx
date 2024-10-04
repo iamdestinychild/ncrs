af6c9be43a807328b24f79a91c17f0d6cdd6db65
iamdestinychild <omoruyidestiny30@gmail.com>
2024-10-04 07:54:32 +0100

    fix

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,54 +1,115 @@
-"useClient"
-import Link from "next/link"
-import DropDown from "./DropDown"
-import styles from '../../public/styling/sidebar.module.css'
-import { useAppContext } from "../context/AppContext"
-import { useEffect, useRef } from "react"
+"useClient";
+import Link from "next/link";
+import DropDown from "./DropDown";
+import styles from "../../public/styling/sidebar.module.css";
+import { useAppContext } from "../context/AppContext";
+import { useEffect, useRef } from "react";
 
 export default function SideBar() {
-    const { sideBarState } = useAppContext()
-    const sideBarRef = useRef()
-    
-    const checkOutFocus = (e) => {
-        if (sideBarRef.current && !sideBarRef.current.contains(e.target)) {
-            sideBarState.slideSideBar(false)
-        }
-    }
+  const { sideBarState } = useAppContext();
+  const sideBarRef = useRef();
 
-    useEffect(() => {
-        document.addEventListener('mousedown', checkOutFocus)
-        return ()=> document.removeEventListener('mousedown', checkOutFocus)
-    })
-    
-    return (
-        <div ref={sideBarRef} className={`${styles.sidebar} ${sideBarState.sideBar?styles.slide_sidebar:``}`}>
-            <ul className={styles.sidebar_inner}>
-                <li><Link href="/">Home</Link></li>
-            <DropDown text="About NCRS">
-                    <ul>
-                        <li><Link href="/about" className={styles.sidebar_dropdown_link}>About Us</Link></li>
-                        <li><Link href="/background" className={styles.sidebar_dropdown_link}>Our Background</Link></li>
-                        <li><Link href="/departments" className={styles.sidebar_dropdown_link}>Department</Link></li>
-                        <li><Link href="management" className={styles.sidebar_dropdown_link}>Managemnet Team</Link></li>
-                        <li><Link href="/organogram" className={styles.sidebar_dropdown_link}>Organogram</Link></li>
-                        <li><Link href="/labs" className={styles.sidebar_dropdown_link}>Laboratories</Link></li>
-                    </ul>
-            </DropDown>
-            <DropDown text="Services">
-                    <ul>
-                        <li><Link href="/services" className={styles.sidebar_dropdown_link}>Our Services</Link></li>
-                        <li><Link href="downloads" className={styles.sidebar_dropdown_link}>Downloads</Link></li>
-                </ul>
-            </DropDown>
-            <DropDown text="Media">
-                    <ul>
-                        <li><Link href="/news" className={styles.sidebar_dropdown_link}>News</Link></li>
-                        <li><Link href="/gallery" className={styles.sidebar_dropdown_link}>Gallery</Link></li>
-                </ul>
-            </DropDown>
-            <li><Link href="/consultancy" className={styles.sidebar_dropdown_link}>Consultancy</Link></li>    
-            <li><Link href="/contact" className={styles.sidebar_dropdown_link}>Contact Us</Link></li>
-        </ul>
-            </div>
-    )
+  const checkOutFocus = (e) => {
+    if (sideBarRef.current && !sideBarRef.current.contains(e.target)) {
+      sideBarState.slideSideBar(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", checkOutFocus);
+    return () => document.removeEventListener("mousedown", checkOutFocus);
+  });
+
+  return (
+    <div
+      ref={sideBarRef}
+      className={`${styles.sidebar} ${
+        sideBarState.sideBar ? styles.slide_sidebar : ``
+      }`}
+    >
+      <ul className={styles.sidebar_inner}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <DropDown text="About NCRS">
+          <ul>
+            <li>
+              <Link href="/about" className={styles.sidebar_dropdown_link}>
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link href="/background" className={styles.sidebar_dropdown_link}>
+                Our Background
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/departments"
+                className={styles.sidebar_dropdown_link}
+              >
+                Department
+              </Link>
+            </li>
+            <Link href="/mandate" className={styles.about_dropdown_link}>
+              Our Mandate
+            </Link>
+            <li>
+              <Link href="management" className={styles.sidebar_dropdown_link}>
+                Managemnet Team
+              </Link>
+            </li>
+            <li>
+              <Link href="/organogram" className={styles.sidebar_dropdown_link}>
+                Organogram
+              </Link>
+            </li>
+            <li>
+              <Link href="/labs" className={styles.sidebar_dropdown_link}>
+                Laboratories
+              </Link>
+            </li>
+          </ul>
+        </DropDown>
+        <DropDown text="Services">
+          <ul>
+            <li>
+              <Link href="/services" className={styles.sidebar_dropdown_link}>
+                Our Services
+              </Link>
+            </li>
+            <li>
+              <Link href="downloads" className={styles.sidebar_dropdown_link}>
+                Downloads
+              </Link>
+            </li>
+          </ul>
+        </DropDown>
+        <DropDown text="Media">
+          <ul>
+            <li>
+              <Link href="/news" className={styles.sidebar_dropdown_link}>
+                News
+              </Link>
+            </li>
+            <li>
+              <Link href="/gallery" className={styles.sidebar_dropdown_link}>
+                Gallery
+              </Link>
+            </li>
+          </ul>
+        </DropDown>
+        <li>
+          <Link href="/consultancy" className={styles.sidebar_dropdown_link}>
+            Consultancy
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact" className={styles.sidebar_dropdown_link}>
+            Contact Us
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
