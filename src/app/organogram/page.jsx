6d1ed93ee0db7cc9a183cf52decc0f162b832d78
iamdestinychild <omoruyidestiny30@gmail.com>
2024-10-04 14:35:21 +0100

    fix

--- src/app/organogram/page.jsx
+++ src/app/organogram/page.jsx
@@ -8,7 +8,7 @@ export default function Organogram(){
                 <div className={styles.hero_layer}></div>
                 <div className={styles.hero_content}>
                     <p className={styles.heading_one}>Home / <span>About NCRS</span></p>
-                    <p className={styles.heading_two}>Our Visions, Mission and Our Team</p>
+                    {/* <p className={styles.heading_two}>Our Visions, Mission and Our Team</p> */}
                     <div className={styles.hero_title_page}>
                         <div className={styles.title_cover}>
                             <p className={styles.title}>NATIONAL CENTRE FOR REMOTE SENSING</p>
@@ -21,7 +21,8 @@ export default function Organogram(){
             </section>
             <section className={styles.section_two}>
                 <div className={styles.section_two_container}>
-                    <h1 className={styles.section_two_container_heading}>ORGANOGRAM</h1>
+                <div className={styles.section_two_content_line}></div>
+                    <h1 className={styles.section_two_container_heading}>Organogram</h1>
                     <div className={styles.section_two_banner_cover}>
                         <Image src="/images/organogram.svg" alt="image" className={styles.section_two_banner} fill />
                     </div>
